b9904a7508e2c3251f54fbb117594478a6f93494
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-06-12 11:36:37 +0530

    Add files via upload

---
+++ getus.c
@@ -0,0 +1,22 @@
+//GETUS IS ALSO USE FOR TAKING INPUT BUT GETUS DOES NOT IGNORE SPACE
+/*#include<stdio.h>
+#include<string.h>
+
+int main () {
+char b[50];
+printf("when we use gets for b:\n");
+gets(b);
+printf("you entered %s\n",b);
+return 0;
+}*/
+
+//scanf THIS FUNCC IGNORE VALUE OF FORWARD OF SPACE
+#include<stdio.h>
+#include<string.h>
+
+int main () {
+char b[50];
+printf("when we use scanf for b:\n");
+printf("you entered %s\n",b);
+return 0;
+}
