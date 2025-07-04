c629213c074c275dea4497ce58206432add9d97d
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-07-04 09:20:00 +0530

    Add files via upload

---
+++ infiniteloop.c
@@ -0,0 +1,24 @@
+/*
+#include<stdio.h>
+
+int main () {
+int i=0;
+while(i<10){
+
+    printf("THE VALUE IS %d \n",i);
+}
+return 0;
+}*/
+#include<stdio.h>
+
+int main () {
+int i=0;
+while(i<20) {
+    if (i>=10){
+        printf("The value is %d\n",i);
+
+    }
+    i++;
+}
+return 0;
+}
