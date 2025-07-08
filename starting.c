fe2b1bcb0141fed619de44260444befd4ade4882
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-07-08 09:53:02 +0530

    starting.c

---
+++ starting.c
@@ -0,0 +1,6 @@
+#indlude<stdio.h> //this is preprocessor directive
+int main(){
+  printf("hey uer plese enter the value of A\n"); //this is for printing any  message
+  scanf("%d",&a); //scanf function is for when we have to take input fro user
+return 0; // return 0 to main function and readyy for execuating  
+}
